67f08cb9e19e22d1c159bd7573194cc50fc14aa6
Ashama <160785162+Ashama23@users.noreply.github.com>
2025-03-10 01:17:07 +0500

    Add files via upload

---
+++ Assignments/class-1/index.py
@@ -0,0 +1,52 @@
+print("Hello World")
+
+
+#Data Types
+
+#String
+#String can be written in three different ways as mention below.
+name1 = "Ashama Abbasi"
+name2 = 'Ashama Abbasi'
+name3 = """Ashama Abbasi"""
+print(name1, name2, name3)
+
+#Integer
+# Integers are whole numbers and it could be positive or negative.
+age = 24
+print(age)
+
+#Float
+# it is a decimal value.
+price = 25.00
+print(price)
+
+#List
+#It a data structure in Python that is a mutable, or changeable, ordered sequence of elements.
+numbers = [24, 26, 95, 30 ]
+fruits = ["apples", "bananas", "strawberries"]
+print(numbers, fruits)
+
+#Boolean and None Type
+age = 25
+old = False
+a = None
+
+print(type(old))
+print(type(a))
+
+#Tuple
+#it is an immutable (unchangeable) collection of various data type elements.
+numbers = [3, 6, 7, 8]
+print(numbers)
+
+# Dictionary
+#mutable data structures that allow you to store key-value pairs.
+book = {"name": "Harry Potter", "author": "J.K Rowling", "pages": 350}
+print(book)
+
+#Set
+#a data type in python used to store several items in a single variable.
+set1 = {2,2,2,3,4,5}
+print(set1)
+
+
